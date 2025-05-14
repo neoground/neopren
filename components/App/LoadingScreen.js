6dbf853d9ee0dc448342d27b2a8a466711e5acfd
Sven Reifschneider <sven@neoground.com>
2025-05-14 11:41:05 +0200

    ✨ Add dynamic loading screen image (changing on each load)

--- components/App/LoadingScreen.js
+++ components/App/LoadingScreen.js
@@ -8,14 +8,22 @@ const html = htm.bind(h)
  */
 export default class LoadingScreen extends Component {
     render(props, state, context) {
+        let style = 'margin-top: 2rem;'
+        if(this.props.fullscreen) {
+            style = 'height: 100vh; overflow: hidden;'
+        }
+
+        let images = ['loader1', 'loader2', 'loader3', 'loader4', 'loader5', 'loader6']
+        let image = images[Math.floor(Math.random()*images.length)]
+
         return html`
-            <div class="container" style="height: 100vh; overflow: hidden;">
+            <div class="container" style=${style}>
                 <div class="d-flex w-100 h-100 align-items-center justify-content-center">
                     <div class="card">
-                        <img src=${window.neopren.baseUrl + '/assets/images/loading.webp'} alt="Loading"
-                             class="card-img-top"/>
+                        <img src=${window.neopren.baseUrl + '/assets/images/loaders/' + image + '.webp'} alt="Loading"
+                             class="card-img-top" />
                         <div class="card-body d-flex align-items-center justify-content-center">
-                            <div class="spinner-border text-primary me-3" role="status"></div>
+                            <div class="spinner-border text-primary me-4" role="status"></div>
                             <h2 class="h2">
                                 Loading...
                             </h2>
